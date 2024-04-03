d7fb594bcf126161621c52cd5b0a043e2d021bde
mpsalunggg <putrasatria893@gmail.com>
2024-04-04 06:05:20 +0800

    add db password

--- configs/db.js
+++ configs/db.js
@@ -4,6 +4,7 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   database: process.env.DB_DATABASES || 'transaction',
+  password: process.env.DB_PASSWORD || '',
   port: process.env.DB_PORT || 8000,
 })
 
