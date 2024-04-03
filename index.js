eccb460aaf74db6dbf493c1812e4d1fae08f3051
mpsalunggg <putrasatria893@gmail.com>
2024-04-04 06:31:35 +0800

    add server port env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const db = require('./configs/db')
 const app = express()
-const port = 3030
+const port = process.env.SERVER_PORT || 3030
 
 app.get('/', (_, res) => {
   db.query('SELECT * FROM user', function (err, results) {
