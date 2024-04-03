6cfe9a733cef4916ddb356e1b7a44c7972699c27
mpsalunggg <putrasatria893@gmail.com>
2024-04-04 07:39:40 +0800

    change to create pool

--- configs/db.js
+++ configs/db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2')
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   database: process.env.DB_DATABASE || 'transaction',
@@ -8,12 +8,16 @@ const db = mysql.createConnection({
   port: process.env.DB_PORT || 8000,
 })
 
-db.connect((err) => {
-  if (err) {
-    console.error('Error connecting to database: ', err)
-    return
-  }
-  console.log('Connected to database')
+// db.connect((err) => {
+//   if (err) {
+//     console.error('Error connecting to database: ', err)
+//     return
+//   }
+//   console.log('Connected to database')
+// })
+db.getConnection((err) => {
+  if (err) console.log(err)
+  console.log('connect db')
 })
 
 module.exports = db
