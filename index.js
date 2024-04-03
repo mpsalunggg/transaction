0c91cfd6a917d5f69f59ee70df63a7786acacad8
mpsalunggg <putrasatria893@gmail.com>
2024-04-04 05:51:36 +0800

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const db = require('./configs/db')
+const app = express()
+const port = 3030
+
+app.get('/', (_, res) => {
+  db.query('SELECT * FROM user', function (err, results) {
+    console.log(results)
+  })
+  res.json({ code: 'Response Success' })
+})
+
+app.listen(port, () => {
+  console.log('Server running on port  ' + port)
+})
